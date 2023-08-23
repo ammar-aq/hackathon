d510f073a8344fedf7b4fbdb3d9dec942eab2748
ammar-aq <ammarkazi.aq@gmail.com>
2023-08-23 19:50:23 +0800

    initialcommit

--- src/Database/Drizzle.ts
+++ src/Database/Drizzle.ts
@@ -3,13 +3,9 @@ import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 
 export const cartColumns = pgTable("cartData", {
-    product_title: varchar(`product_title`).notNull(),
+    product_title: varchar("product_title").notNull(),
     product_url: varchar("product_url").notNull(),
     product_price: integer("product_price").notNull(),
-    
-    // product_title: varchar("product_title",{length: 255}).notNull(),
-    // product_url: varchar().notNull(),
-    // product_price: integer().notNull(),
 });
 
 export const db = drizzle(sql);
