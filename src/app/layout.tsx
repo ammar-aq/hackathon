b273ceef4ad5a96f9ca09305cf8e51694a875ea2
ammar-aq <ammarkazi.aq@gmail.com>
2023-08-31 08:08:24 +0800

    Issue_implementing_Button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <ClerkProvider>
     <html lang="en">
+      <Header/>
       <body className={inter.className}>{children}</body>
     </html>
+    </ClerkProvider>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+// "use client";
+import React from "react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+// import { useAuth } from "@clerk/nextjs";
+
+export default function Header() {
+  return (
+    <div>
+      {/* <SignInButton>  */}
+        <button>Sign In</button>
+        {/* </SignInButton> */}
+     
+    </div>
+    
+  )
+}
+
